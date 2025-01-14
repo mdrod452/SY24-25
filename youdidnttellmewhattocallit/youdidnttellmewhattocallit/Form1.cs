ccf80bbede9e8ad08d228cbcf97430d106a6b9ef
milo <mdrod452@learner.dvusd.org>
2025-01-14 08:54:42 -0700

    added binary project

---
+++ youdidnttellmewhattocallit/youdidnttellmewhattocallit/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace youdidnttellmewhattocallit
+{
+    public partial class Form1 : Form
+    {
+        int total = 0;
+        int[] bits = new int[8];
+
+
+        private void Calc()
+        {
+            if (bits[0] != 0) total += 1;
+            if (bits[1] != 0) total += 2;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+            if (bits[0] != 0) total += 1;
+
+        }
+    }
+}
